5d093a7e8723e862132343835fb6e6a773e33984
hamzazirari <hamzazirari6002@gmail.com>
2026-02-25 12:20:39 +0000

    feat : add user and colocation migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,11 +17,14 @@ class User extends Authenticatable
      *
      * @var list<string>
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+protected $fillable = [
+    'name',
+    'username',
+    'email',
+    'password',
+    'photo',
+    'reputation',
+];
 
     /**
      * The attributes that should be hidden for serialization.
